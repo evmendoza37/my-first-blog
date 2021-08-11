075c65678b58106a7354b54825c8871879328a39
evmendoza37 <45073641+evmendoza37@users.noreply.github.com>
2021-08-10 21:13:04 -0500

    added period to pythonanywhere

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%h!b87ggq%1tu+h5jtm^mum=necu(s9wea(a3q^%x5ipuir#@r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
